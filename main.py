1d7a0f478020f58897da239a86d973c8efaca814
wiered <wieredsh@gmail.com>
2022-07-29 03:06:19 +0300

    v1.3 	modified:   main.py 	modified:   newmanga.py 	modified:   remanga.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from newmanga import add_multiple, add_multiple_crazy_method, add_multiple_lazy_method, get_name, add_manga
-import keyboard
+from newmanga import migrate
 import requests, json
+import remanga
 
-login = input("Input newmanga login: ")
-password = input("Input newmanga password: ")
-from remanga import get_manga
+bookmarks = {
+    1: "Читаю",
+    2: "Буду читать",
+    3: "Прочитано",
+    4: "Отложено",
+    5: "Брошено",
+    6: "Не интересно"
+}
 
 def main():
     login_url = 'https://api.newmanga.org/v2/login'
+
+    login = input("Input newmanga login: ")
+    password = input("Input newmanga password: ")
     login_payload = {
         'credentials': login,
         'password': password
@@ -18,38 +26,14 @@ def main():
     if p.status_code != 200:
         print("Что-то пошло не так, попробуйте проверить правильность введённых данных")
         return None
-    
-    reading_now, will_read, read, postponed, throw, not_interedted = get_manga() # Получение списков из решки
-    
-    print('Выберете пункт:')
-    print("1 - Я хочу сам всё проверить (Рекомендую для маленькой библиотеки из 50-200 манг)\n    - Покажет вам всё что есть в виде:\n      Remanga name: {remanga name}\n      Newmanga name: {newmanga name}\n      По нажатию 'y' или 'enter' добавит в библиотеку, по нажатию любой другой клавиши - нет")
-    print()
-    print("2 - Я ленивый, давай автоматически (Рекомендую для большой библиотеки из 300+ манг)\n    - Ленивый метод не требующий от юзера лишних телодвижений.\n      Возможно, некоторая манга не будет добавлена")
-    print()
-    print("3 - Я бешенный, давай всё подряд (Не рекомендую)\n    - Бешенный метод не требующий от юзера лишних телодвижений.\n      Возможно, будет добавлена манга не из библиоеки")
 
-    k = keyboard.read_event().scan_code
-    if k == 3:
-        add_multiple_lazy_method(login_payload, reading_now, "Читаю")
-        add_multiple_lazy_method(login_payload, will_read, "Буду читать")
-        add_multiple_lazy_method(login_payload, read, "Прочитано")
-        add_multiple_lazy_method(login_payload, postponed, "Отложено")
-        add_multiple_lazy_method(login_payload, throw, "Брошено")
-        add_multiple_lazy_method(login_payload, not_interedted, "Не интересно")
-    elif k == 4:
-        add_multiple_crazy_method(login_payload, reading_now, "Читаю")
-        add_multiple_crazy_method(login_payload, will_read, "Буду читать")
-        add_multiple_crazy_method(login_payload, read, "Прочитано")
-        add_multiple_crazy_method(login_payload, postponed, "Отложено")
-        add_multiple_crazy_method(login_payload, throw, "Брошено")
-        add_multiple_crazy_method(login_payload, not_interedted, "Не интересно")
-    else:
-        add_multiple(login_payload, reading_now, "Читаю")
-        add_multiple(login_payload, will_read, "Буду читать")
-        add_multiple(login_payload, read, "Прочитано")
-        add_multiple(login_payload, postponed, "Отложено")
-        add_multiple(login_payload, throw, "Брошено")
-        add_multiple(login_payload, not_interedted, "Не интересно")
+    remanga.load_webdriver()
+
+    parsed_manga = remanga.get_manga() # Получение списков из решки
+    bookmark = 1
+    for catalogue in parsed_manga:
+        migrate(login_payload, catalogue, bookmarks[bookmark])
+        bookmark += 1
     
 if __name__ == '__main__':
     main()
--- remanga.py
+++ remanga.py
@@ -1,27 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import keyboard
 
+import time
 import logging
 logger = logging.getLogger('WDM')
 logger.propagate = False
 logger.disabled = True
 
-import time
-
-CHROME_PATH = './chrome/chrome'
-CHROMEDRIVER_PATH = './chromedriver'
-WINDOW_SIZE = "1080,900"
-
-options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
-options.add_argument("--log-level=3")
-options.add_argument("--window-size=%s" % WINDOW_SIZE)
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = None
 
 def main():
     driver.get("https://remanga.org/manga")
@@ -43,12 +32,22 @@ def main():
     manga_list = manga_list.text.split('\n')
     manga_list = manga_list[1:len(manga_list):2]
 
-def get_list(list, btn_xpath, xpath):
+def load_webdriver(headless: bool = False):
+    global driver
+    WINDOW_SIZE = "1080,900"
+
+    options = webdriver.ChromeOptions()
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("--log-level=3")
+    options.add_argument("--window-size=%s" % WINDOW_SIZE)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+def scroll_to_bottom():
     """
-    --- Getting manga lists form remanga
+    --- Scrolling down to bottom of the page
     """
-    btn = driver.find_element(By.XPATH, value = btn_xpath)
-    btn.click()
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -57,34 +56,41 @@ def get_list(list, btn_xpath, xpath):
         if new_height == last_height:
             break
         last_height = new_height
-    manga_list = driver.find_element(By.XPATH, value = xpath)
-    manga_list = manga_list.text.split('\n')
-    manga_list = manga_list[1:len(manga_list):2]
-    return manga_list
-    
 
+def parse_catalogue(button):
+    """
+    --- Parsing manga catalogue form remanga
+    """
+    catalogue_xpath = '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div'
+    button_xpath = '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[' + button + ']'
+    button = driver.find_element(By.XPATH, value = button_xpath)
+    button.click()
+    scroll_to_bottom()
+    parsed_catalogue = driver.find_element(By.XPATH, value = catalogue_xpath)
+    parsed_catalogue = parsed_catalogue.text.split('\n')
+    parsed_catalogue = parsed_catalogue[1:len(parsed_catalogue):2]
+    return parsed_catalogue
+    
 def get_manga():
+    """
+    --- Parsing manga catalogues form remanga account
+    """
     driver.get("https://remanga.org/manga")
     driver.implicitly_wait(8)
-
     print("Зайдите в свой аккаунт на сайте, после чего нажмите Enter в консоли...")
     keyboard.wait('Enter')
-    user_btn_xpath = '//*[@id="app"]/header/div/button[2]/span[1]/span/div/img'
-    user_btn = driver.find_element(By.XPATH, value = user_btn_xpath)
-    user_btn.click()
-    user_btn_xpath = '//*[@id="menu-list"]/div/ul/li[1]/a'
-    user_btn = driver.find_element(By.XPATH, value = user_btn_xpath)
-    user_btn.click()
 
-    reading_now = get_list('Читаю', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[1]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
-    will_read = get_list('Буду читать', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[2]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
-    read = get_list('Прочитано', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[3]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
-    postponed = get_list('Отложил', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[4]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
-    throw = get_list('Бросил', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[5]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
-    not_interedted = get_list('Не интересно', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/button[6]', '//*[@id="app"]/div/div[2]/div[1]/div[2]/div/div[2]/div')
+    button_xpath = '//*[@id="app"]/header/div/button[2]/span[1]/span/div/img'
+    button = driver.find_element(By.XPATH, value = button_xpath)
+    button.click()
 
+    button_xpath = '//*[@id="menu-list"]/div/ul/li[1]/a'
+    button = driver.find_element(By.XPATH, value = button_xpath)
+    button.click()
+
+    parsed_manga = [parse_catalogue(x) for x in range(1,7)]
     driver.close()
-    return reading_now, will_read, read, postponed, throw, not_interedted
+    return parsed_manga
 
 if __name__ == '__main__':
     main()
